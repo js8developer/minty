d1e721f919ae16431f515c55d3962ae6f50338a8
JasonSchneider8 <36863375+JasonSchneider8@users.noreply.github.com>
2022-02-18 14:23:21 -0500

    Add test deploying wallet

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,9 @@ require("@nomiclabs/hardhat-waffle");
 const dotenv = require('dotenv');
 dotenv.config();
 
+const fs = require("fs");
+const deployingWalletPrivateKey = fs.readFileSync(".secret").toString() // Test wallet... no real monies
+
 module.exports = {
   solidity: "0.8.4",
   networks: {
@@ -10,10 +13,12 @@ module.exports = {
       chainId: 1337
     },
     mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [deployingWalletPrivateKey]
     },
     mainnet: {
-      url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT_ID}`
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [deployingWalletPrivateKey]
     }
   },
 };
